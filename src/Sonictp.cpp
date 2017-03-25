867a8cada7dd3961c666ade966758ebe980e117c
Julieta Ponce <jbponce36@gmail.com>
2017-03-25 16:32:48 -0300

    Agregue el constructor de entidades. Recibe la lista de entidades desde el json.

--- src/Sonictp.cpp
+++ src/Sonictp.cpp
@@ -11,18 +11,27 @@ using namespace std;
 #include "jventana.h"
 #include "Rectangulo.h"
 #include "Circulo.h"
+#include "jescenario.h"
+#include "ConstructorEntidades.h"
+
 using namespace std;
 
 int main() {
 
 	parseadorJson* parseador = new parseadorJson();
 	//jescenarioJuego* jparseador = parseador.
-	char* file= "configuracion/f.json";
+	char* file= "src/f.json";
 
     jescenarioJuego* jparseador = parseador->parsearArchivo(file);
 
-	VistaSDL *vista = new VistaSDL(jparseador->getVentana(),jparseador->getConfiguracion(),jparseador->getEscenario());
-	vista->cargarTexturas();
+
+    VistaSDL *vista = new VistaSDL(jparseador->getVentana(),jparseador->getConfiguracion(),jparseador->getEscenario());
+
+    list<Rectangulo> rectangulos;
+    list<Circulo> circulos;
+    ConstructorEntidades constructorEntidades = ConstructorEntidades(jparseador->getEscenario(), &rectangulos, &circulos);
+
+    vista->cargarTexturas();
 	vista->mostrarVentana();
 	vista->cerrar();
 
