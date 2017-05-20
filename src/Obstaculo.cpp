7212c1a04237b0692844ca21579c14a71d6b07bc
patodimasi <pdimasi@hotmail.com>
2017-05-20 01:31:37 -0300

    creacion de clase obstaculo

---
+++ src/Obstaculo.cpp
@@ -0,0 +1,18 @@
+/*
+ * Obstaculo.cpp
+ *
+ *  Created on: 20 may. 2017
+ *      Author: patricia
+ */
+
+#include "Obstaculo.h"
+
+Obstaculo::Obstaculo() {
+	// TODO Auto-generated constructor stub
+
+}
+
+Obstaculo::~Obstaculo() {
+	// TODO Auto-generated destructor stub
+}
+
---
+++ src/Obstaculo.h
@@ -0,0 +1,31 @@
+/*
+ * Obstaculo.h
+ *
+ *  Created on: 20 may. 2017
+ *      Author: patricia
+ */
+
+#ifndef OBSTACULO_H_
+#define OBSTACULO_H_
+#include <string>
+
+class Obstaculo {
+public:
+	Obstaculo();
+	virtual ~Obstaculo();
+	virtual void setruta(std::string) = 0;
+	virtual std::string getruta() = 0;
+
+	virtual void setancho(int) = 0;
+	virtual int getancho() = 0;
+
+	virtual void setalto(int) = 0;
+	virtual int getalto() = 0;
+private:
+
+	std::string ruta;
+	int ancho;
+	int alto;
+};
+
+#endif /* OBSTACULO_H_ */
