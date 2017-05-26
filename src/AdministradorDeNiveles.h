c0413c49be0cdd92a16bfd33abba306db25f068e
villanicr <vilanicr@gmail.com>
2017-05-26 19:00:29 -0300

    agrego clase

---
+++ src/AdministradorDeNiveles.h
@@ -0,0 +1,18 @@
+/*
+ * AdministradorDeNiveles.h
+ *
+ *  Created on: 22 may. 2017
+ *      Author: cristian
+ */
+
+#ifndef ADMINISTRADORDENIVELES_H_
+#define ADMINISTRADORDENIVELES_H_
+#include "VistaSDL.h"
+
+class AdministradorDeNiveles {
+
+	public:
+	AdministradorDeNiveles();
+
+};
+#endif /* ADMINISTRADORDENIVELES_H_ */
---
+++ src/AdminsitradorDeNiveles.cpp
@@ -0,0 +1,16 @@
+/*
+ * AdminsitradorDeNiveles.cpp
+ *
+ *  Created on: 22 may. 2017
+ *      Author: cristian
+ */
+
+#include "AdministradorDeNiveles.h"
+
+
+
+AdministradorDeNiveles::AdministradorDeNiveles(){
+
+}
+
+
